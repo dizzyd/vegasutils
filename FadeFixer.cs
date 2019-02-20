785e20eac8e335bd69a909ddfb8f2e5a1446ce36
Dave Smith <dizzyd@dizzyd.com>
2019-02-20 12:51:27 -0700

    Adding initial files

---
+++ FadeFixer.cs
@@ -0,0 +1,23 @@
+﻿using ScriptPortal.Vegas;
+
+namespace FadeFixer
+{
+    public class EntryPoint
+    {
+        public void FromVegas(Vegas vegas)
+        {
+            foreach (Track t in vegas.Project.Tracks)
+            {
+                foreach (TrackEvent evt in t.Events)
+                {
+                    if (evt.Start > Timecode.FromSeconds(0))
+                    {
+                        evt.FadeIn.Length = Timecode.FromSeconds(1);
+                    }
+                    evt.FadeOut.Length = Timecode.FromSeconds(1);
+                }
+            }
+        }
+    }
+}
+
